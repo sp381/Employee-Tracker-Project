ce865d6204eaa6a54f3000023fe24ba7ba26c452
sp381 <sarah.powell381@gmail.com>
2022-04-21 16:15:12 -0700

    added view, add and update functions

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cTable = require('console.table');
 const connection = mysql.createConnection(
     {
         host: "localhost",
+        port: 3001,
         user: "root",
         password: "root",
         database: "employee_tracker",
@@ -16,4 +17,51 @@ connection.connect((err) => {
     console.log('you are successful!');
 });
 
+// Create a function to view, add and update information
+function employeeTracker() { 
+    inquirer.prompt({
+        name: 'Choices',
+        type: 'list',
+        message: 'Choose from the Following Options: ',
+        choices: [
+            'View All Departments',
+            'View All Roles', 
+            'View All Employees', 
+            'Add A Department',
+            'Add A Role',
+            'Add An Employee', 
+            'Update an Employee Role',
+            'Exit'
+        ]
+    }).then(answers => {
+        switch (answers.action) {
+            case 'View All Departments':
+                viewDepartments();
+                break;
+        
+            case 'View All Roles':
+                viewRoles();
+                break;
 
+            case 'View All Employees':
+                viewEmployees();
+                break; 
+
+            case 'Add A Department':
+                addDepartment();
+                break;  
+                
+            case 'Add A Role':
+                addRole();
+                break;
+            
+            case 'Add An Employee':
+                addEmployee();
+                break;        
+            
+            case 'Update an Employee Role':
+                updateEmployee();
+                break;
+        }
+    });
+}
