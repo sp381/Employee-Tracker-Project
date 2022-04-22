e06c814369ea7d17113b8b753420d60ad2f97470
sp381 <sarah.powell381@gmail.com>
2022-04-22 10:25:10 -0700

    added inquirer to the 'add section'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-
+const util = require("util");
 const connection = mysql.createConnection(
     {
         host: "localhost",
@@ -14,8 +14,16 @@ const connection = mysql.createConnection(
 
 connection.connect((err) => {
     if(err) throw err;
-    console.log('you are successful!');
+    console.log(err);
+    res.status(500);
+    return res.send("There was an error with connecting.");
+    console.log('You successfully connected!');
+
+//Create a function to allow inquirer to prompt data
+employeeTracker();
 });
+
+connection.query = util.promisfy(connection.query);
 
 // Create a function to view, add and update information
 function employeeTracker() { 
@@ -30,35 +38,40 @@ function employeeTracker() {
             'Add A Department',
             'Add A Role',
             'Add An Employee', 
+            'Remove An Employee',
             'Update an Employee Role',
             'Exit'
         ]
+
     }).then(responses => {
         switch (responses.action) {
             case 'View All Departments':
                 viewDepartment();
                 employeeTracker();
+                
                 break;
         
             case 'View All Roles':
                 viewRoles();
                 employeeTracker();
+                
                 break;
 
             case 'View All Employees':
                 viewEmployees();
                 employeeTracker();
+                
                 break; 
 
             // Start new case "Add Department"
-            case 'Add A Department':
+            case "Add A Department":
                 inquirer.prompt ([
                     {
                         type: 'input',
                         name: 'department',
                         message: 'Add the department here: ',
                         validate: responses => {
-                            if (responses) {
+                            if (responses !== "") {
                                 return true;
                             } else {
                                 console.log('Please enter the department you would like to add.');
@@ -66,27 +79,31 @@ function employeeTracker() {
                             }
                         },
                     },
-                ]).then(answers => {
-                    addDepartment(answers.Department);
+                ]).then(responses => {
+                    addDepartment(responses.department);
                     employeeTracker();
-                })
-                break;  
+                })    
+
             //Start a new case "Add Role"    
-            case 'Add A Role':
+            case "Add A Role":
+                // const sql = 'SELECT * FROM department';
+                // connection.query(sql, (err, results) => {
+                //     if(err) throw(err);
+                // })
                 inquirer.prompt([
                     {
                         type: 'input',
                         name: 'role',
-                        message: 'Please enter the title role.'
+                        message: 'Please enter the title role.',
                         validate: responses => {
-                            if (responses) {
+                            if (responses !== "") {
                                 return true;
                             } else {
                                 console.log('Please enter the title of the new role.');
                                 return false
                             }
-                        }
-
+                        },
+                    },
                         {
                             type: 'input',
                             name: 'salary',
@@ -97,25 +114,74 @@ function employeeTracker() {
                             type: 'input',
                             name: 'department_id',
                             message: 'Please enter the department id.',
-                        }
-                    }
+                        },
+                    
 
                 ]).then(responses => {
                     addRole(responses.title, responses.salary, responses.department_id);
                     employeeTracker();
                 })
+
                 break;
-            
-            case 'Add An Employee':
-                addEmployee();
+
+            //Start a new case
+            case "Add An Employee":
+                inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'first_name',
+                            message: 'Enter their first name here',
+                        },
+
+                        {
+                            type: 'input',
+                            name: 'last_name',
+                            message: 'Enter their last name here',
+                        },
+
+                        {
+                            type: 'input',
+                            name: 'role_id',
+                            message: 'Please enter their title.',
+                        },
+
+                        {
+                            type: 'input',
+                            name: 'manager_id',
+                            message: 'Please enter the manager id.',
+                        },
+
+                ]).then(responses => {
+                    addEmployee(responses.first_name, responses.last_name, responses.role_id, responses.manager_id);
+                    employeeTracker();
+                })
+
                 break;        
             
-            case 'Update an Employee Role':
-                updateEmployee();
+                case "Update an Employee's Role":
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'manager',
+                        message: "Please enter the Employee's role."
+                    },
+
+                    {
+                        type: 'input',
+                        name: 'id',
+                        message: "Please enter the Employee's ID",
+                    }
+                ]).then(responses => {
+                    updateEmployee(responses.manager, responses.id);
+                    employeeTracker();
+                })
+
                 break;
         }
 
 
     });
 }
+
+
 employeeTracker()
