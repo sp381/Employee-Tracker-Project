68a662c4babf7f9e51dd92f5ac63e9f0f542e228
sp381 <sarah.powell381@gmail.com>
2022-04-22 11:24:58 -0700

    updated seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,15 +9,16 @@ VALUES
 INSERT INTO role(id, title, salary, deparment_id)
 VALUES
 (1, "HR Head Rep", 80000, 1),
-(2, "HR Agent Rep", 70000, 1),
+(2, "Customer Service", 50000, 1),
 (3, "Manager", 85000, 2),
-(4, "Assist Manager", 70000, 2),
+(4, "Assist to the Regional Manager", 60000, 2),
 (5, "Sales Rep", 100000, 3),
 (6, "Sales Assistant", 65000, 3);
 
 INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
 VALUES
-(1, "sarah", "powell", 1, NULL),
-(2, "bob", "jones", 2, 1),
-(3, "eric", "martin", 2, 2),
-(4, "greg", "long", 3, 1);
+(1, "Toby", "Flenderson", 1, NULL),
+(2, "Kelly", "Kapoor", 2, 3),
+(3, "Michael", "Scott", 2, 1),
+(4, "Dwight", "Schrute", 2, 2);
+(5, "Jim", "Halpert", 3, 2)
