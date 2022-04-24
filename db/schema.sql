a8fe1e87d19f6a0f7eb2b884c3dd427222721439
sp381 <sarah.powell381@gmail.com>
2022-04-24 11:53:49 -0700

    fixed line 19 in schema

--- db/schema.sql
+++ db/schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY, 
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR(30),
     role_id INT, 
